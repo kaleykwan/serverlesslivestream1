2c7ec00dee108e073b6854f64c9097d8c48cad8c
Kaley Kwan <kaleymkwan@gmail.com>
2021-06-23 17:21:46 -0700

    finish twocatz

--- twocatz/index.js
+++ twocatz/index.js
@@ -3,20 +3,41 @@ const fetch = require('node-fetch')
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let resp = await fetch("https://cataas.com/cat/cute/says/Serverless", {
-        method: 'GET'
-    });
+    async function getCat() {
+        let resp = await fetch("https://251a9d93-8c22-472d-b5c5-c0b013b15125.mock.pstmn.io/bitproject", {
+            method: 'GET'
+        });
+        
+        let data = await resp.arrayBuffer()
+        // we need to receive it as a buffer since this is an image we are receiving from the API
+        // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob    
+        var base64data = Buffer.from(data).toString('base64')
+        //put what you want to turn into base64 inside "originaldata"
+        //"originaldata" will be encoded in base64. 
+        
+        return base64data
+    }
     
-    let data = await resp.arrayBuffer()
-    // we need to receive it as a buffer since this is an image we are receiving from the API
-    // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
-    
-    var base64data = Buffer.from(data).toString('base64')
-//put what you want to turn into base64 inside "originaldata"
-//"originaldata" will be encoded in base64.
+    let catpic1 = await getCat()
+    let catpic2 = await getCat()
+
+    async function getNames() {
+        var names = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"]
+        var random_value = Math.floor(names.length * Math.random())
+        var resultname = names[random_value]
+
+        return resultnames
+    }
+
+    let name1 = getNames()
+    let name2 = getNames()
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: {base64data}
+        body: {
+            cat1: catpic1
+            cat2: catpic2
+            names: [name1, name2]
+        }
     };
 }
