2c689e7111d42a3fdc602ccdf6e8b01e17bd5437
Kaley Kwan <kaleymkwan@gmail.com>
2021-06-29 21:21:13 -0700

    update function

--- song4u/index.js
+++ song4u/index.js
@@ -4,7 +4,8 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const queryObject = querystring.parse(req.body);
-
+    //random comment
+    
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: queryObject.MediaUrl0
