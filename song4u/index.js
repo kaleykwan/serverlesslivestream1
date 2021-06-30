e44e8c3f371b493320145a89b708e27e43bba65b
Kaley Kwan <kaleymkwan@gmail.com>
2021-06-30 11:54:58 -0700

    age function

--- song4u/index.js
+++ song4u/index.js
@@ -5,9 +5,63 @@ module.exports = async function (context, req) {
 
     const queryObject = querystring.parse(req.body);
     //random comment
-    
+
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: queryObject.MediaUrl0
     };
 }
+
+function downloadImage() {
+    let resp = await fetch("https://hackervoicee1.azurewebsites.net/api/song4u?code=A1O/W2eNLaSzs326aLuKXfKd/Y0rODgEJRH64ZndscBacoknHa1lRA==", {
+        method: 'GET',
+    })
+
+    // receive the response
+    let data = await resp.arrayBuffer()
+    // we are receiving it as a Buffer since this is binary data
+}
+   
+async function analyzeImage(img){
+    const subscriptionKey = process.env.SUBSCRIPTIONKEY;
+    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
+    
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'age'
+    })
+    
+    // making the post request
+    let resp = await fetch(uriBase + '?' + params.toString(),{
+        method: 'POST',
+        body: img,
+        // img is the parameter inputted
+        headers: {
+            'Content-Type' : 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key' : subscriptionKey
+        }
+    })
+    
+    // receive the response
+    let data = await resp.json();
+    
+    let age = result[0].faceAttributes.age
+
+    if (age >=5 && age <=25) {
+        id = "GenZ"
+    } else if (age >=26 && age <=41) {
+        id = "GenY"
+    } else if (age >=42 && age <=57) {
+        id = "GenX"
+    } else if (age >=58 && age <=76) {
+        id = "BabyBoomers"
+    } else {
+        id = "Unknown"
+    }
+
+    return id;
+}
+    
+
+
+
