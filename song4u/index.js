9bd05dc4bbb737e968fe0e94b9d00ffceec7aed0
Kaley Kwan <kaleymkwan@gmail.com>
2021-07-01 10:43:40 -0700

    update age function

--- song4u/index.js
+++ song4u/index.js
@@ -1,4 +1,5 @@
 const querystring = require('querystring');
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
